2981148cc3655ad1df8fd94e6a6d390ac853c09e
HugooVer <hugo.vercelloneg@gmail.com>
2024-01-16 17:34:28 +0100

    ex00

---
+++ CPP_Module_03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+
+class ClapTrap
+{
+private:
+	std::string _name;
+	int _hp;
+	int _energy;
+	int _damage;
+
+public:
+	ClapTrap();
+	ClapTrap(std::string _name);
+	ClapTrap(ClapTrap const &clapTrap);
+	~ClapTrap();
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
