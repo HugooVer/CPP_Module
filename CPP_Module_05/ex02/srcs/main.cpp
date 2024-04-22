2c869656124d1827b8089ced8fa8aa5a13dbb4c5
HugooVer <hugo.vercelloneg@gmail.com>
2024-04-22 20:35:44 +0200

    good main

--- CPP_Module_05/ex02/srcs/main.cpp
+++ CPP_Module_05/ex02/srcs/main.cpp
@@ -1,55 +1,54 @@
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main() {
-
-	// try
-	// {
-	// 	Form Test;
-	// 	std::cout << Test << std::endl;
-	// }
-	// catch (std::exception& e)
-	// {
-	// 	std::cout << e.what() << std::endl;
-	// }
-
-	// try
-	// {
-	// 	std::cout << "\n\n";
-	// 	Bureaucrat good("Good", 10);
-	// 	std::cout << good << std::endl;
-	// 	Form test("Test", 10, 10);
-	// 	std::cout << test << std::endl;
-	// 	test.signForm(good);
-	// 	std::cout << test << std::endl;
-	// 	test.signForm(good);
-	// 	std::cout << test << std::endl;
-	// }
-	// catch (std::exception& e)
-	// {
-	// 	std::cout << e.what() << std::endl;
-	// }
-	
-	// try
-	// {
-	// 	std::cout << "\n\n";
-	// 	Bureaucrat bad("Bad", 12);
-	// 	std::cout << bad << std::endl;
-	// 	Form test("Test", 10, 10);
-	// 	std::cout << test << std::endl;
-	// 	test.signForm(bad);
-	// 	std::cout << test << std::endl;
-	// 	bad.incr();
-	// 	test.signForm(bad);
-	// 	std::cout << test << std::endl;
-	// 	bad.incr();
-	// 	test.signForm(bad);
-	// 	std::cout << test << std::endl;
-	// }
-	// catch (std::exception& e)
-	// {
-	// 	std::cout << e.what() << std::endl;
-	// }
+	try
+	{
+		Bureaucrat bad("bad", 150);
+		Bureaucrat good("good", 1);
+		ShrubberyCreationForm tree("tree");
+		RobotomyRequestForm robo("robo");
+		PresidentialPardonForm pre("pre");
+		std::cout << std::endl;
+		tree.signAForm(bad);
+		robo.signAForm(bad);
+		pre.signAForm(bad);
+		std::cout << std::endl;
+		tree.execute(good);
+		robo.execute(good);
+		pre.execute(good);
+		std::cout << std::endl;
+		tree.execute(bad);
+		robo.execute(bad);
+		pre.execute(bad);
+		std::cout << std::endl;
+		tree.signAForm(good);
+		robo.signAForm(good);
+		pre.signAForm(good);
+		std::cout << std::endl;
+		tree.execute(bad);
+		robo.execute(bad);
+		pre.execute(bad);
+		std::cout << std::endl;
+		tree.execute(good);
+		robo.execute(good);
+		tree.execute(good);
+		robo.execute(good);
+		tree.execute(good);
+		robo.execute(good);
+		tree.execute(good);
+		robo.execute(good);
+		std::cout << std::endl;
+		pre.execute(good);
+		std::cout << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
