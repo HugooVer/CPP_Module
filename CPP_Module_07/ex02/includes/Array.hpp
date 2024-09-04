dba6f1637337f14a2a4123b172594cd002285749
HugooVer <hugo.vercelloneg@gmail.com>
2024-09-04 20:44:22 +0200

    work in progress

--- CPP_Module_07/ex02/includes/Array.hpp
+++ CPP_Module_07/ex02/includes/Array.hpp
@@ -1,34 +1,100 @@
+// #pragma once
+
+// #include <iostream>
+// #include <cstdlib>
+// #include <string>
+// #include <sstream>
+// #include <stdint.h>
+
+// template <class T>
+// class Array
+// {
+// 	private:
+// 		Array() : T arr[];
+// 		Array(unsigned int n) : T arr[n]
+// 		{
+// 			for (unsigned int i = 0, i < n, ++i)
+// 				arr[i] = new T();
+// 		}
+// 		Array(Array const &array);
+// 		~Array();
+
+// 	public:
+// 		int size();
+
+// 		Array& operator= (Array const &array);
+// 		const Type & operator[](int Index) const
+// 		{
+// 			return Value[Index];
+// 		}
+// 		Type & operator[](int Index)
+// 		{
+// 			return Value[Index];
+// 		}
+// };
+
+
 #pragma once
 
 #include <iostream>
 #include <cstdlib>
-#include <string>
-#include <sstream>
-#include <stdint.h>
+#include <exception>
 
 template <class T>
 class Array
 {
-	private:
-		Array() : T arr[];
-		Array(unsigned int n) : T arr[n]
-		{
-			for (unsigned int i = 0, i < n, ++i)
-				arr[i] = new T();
-		}
-		Array(Array const &array);
-		~Array();
+private:
+	T* arr;
+	unsigned int _size;
 
-	public:
-		int size();
+public:
+	Array() : arr(NULL), _size(0) {}
 
-		Array& operator= (Array const &array);
-		const Type & operator[](int Index) const
-		{
-			return Value[Index];
+	Array(unsigned int n) : arr(new T[n]()), _size(n)
+	{
+		for (unsigned int i = 0; i < n; ++i)
+			arr[i] = T();
+	}
+
+	Array(Array const& other) : arr(new T[other._size]), _size(other._size)
+	{
+		for (unsigned int i = 0; i < _size; ++i)
+			arr[i] = other.arr[i];
+	}
+
+	~Array()
+	{
+		delete[] arr;
+	}
+
+	Array& operator=(Array const& other)
+	{
+		if (this != &other) {
+			delete[] arr;
+			_size = other._size;
+			arr = new T[_size];
+			for (unsigned int i = 0; i < _size; ++i)
+				arr[i] = other.arr[i];
 		}
-		Type & operator[](int Index)
-		{
-			return Value[Index];
+		return *this;
+	}
+
+	T& operator[](int index)
+	{
+		if (index < 0 || index >= static_cast<int>(_size))
+			throw std::out_of_range("Array index out of bounds");
+		return arr[index];
+	}
+
+	const T& operator[](int index) const
+	{
+		if (index < 0 || index >= static_cast<int>(_size))
+			throw std::out_of_range("Array index out of bounds");
+		return arr[index];
+	}
+
+	unsigned int size() const
+	{
+		return _size;
 		}
 };
--- CPP_Module_07/ex02/srcs/main.cpp
+++ CPP_Module_07/ex02/srcs/main.cpp
@@ -1,2 +1,43 @@
-#include <Array.hpp>
+#include <iostream>
+#include "Array.hpp"
 
+int main() {
+    // Test default constructor
+    Array<int> arr1;
+    std::cout << "arr1 size: " << arr1.size() << std::endl; // Expected output: 0
+
+    // Test constructor with size
+    Array<int> arr2(5);
+    std::cout << "arr2 size: " << arr2.size() << std::endl; // Expected output: 5
+
+    // Test element assignment and access
+    for (unsigned int i = 0; i < arr2.size(); ++i) {
+        arr2[i] = i * 11;
+    }
+    for (unsigned int i = 0; i < arr2.size(); ++i) {
+        std::cout << "arr2[" << i << "]: " << arr2[i] << std::endl; // Expected output: 0, 10, 20, 30, 40
+    }
+
+    // Test copy constructor
+    Array<int> arr3(arr2);
+    std::cout << "arr3 (copy of arr2) size: " << arr3.size() << std::endl; // Expected output: 5
+    for (unsigned int i = 0; i < arr3.size(); ++i) {
+        std::cout << "arr3[" << i << "]: " << arr3[i] << std::endl; // Expected output: 0, 10, 20, 30, 40
+    }
+
+    // Test assignment operator
+    Array<int> arr4 = arr3;
+    std::cout << "arr4 (assigned from arr3) size: " << arr4.size() << std::endl; // Expected output: 5
+    for (unsigned int i = 0; i < arr4.size(); ++i) {
+        std::cout << "arr4[" << i << "]: " << arr4[i] << std::endl; // Expected output: 0, 10, 20, 30, 40
+    }
+
+    // Test bounds checking (should throw an exception)
+    try {
+        std::cout << "arr2[10]: " << arr2[10] << std::endl; // Out of bounds, should throw an exception
+    } catch (std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl; // Expected output: Exception caught: Array index out of bounds
+    }
+
+    return 0;
+}
