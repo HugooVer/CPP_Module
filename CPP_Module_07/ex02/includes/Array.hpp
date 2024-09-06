54fb8ec7cb5632ba2fa0518a37897b7a206dc412
HugooVer <hugo.vercelloneg@gmail.com>
2024-09-06 17:27:38 +0200

    good Array

--- CPP_Module_07/ex02/includes/Array.hpp
+++ CPP_Module_07/ex02/includes/Array.hpp
@@ -1,39 +1,3 @@
-// #pragma once
-
-// #include <iostream>
-// #include <cstdlib>
-// #include <string>
-// #include <sstream>
-// #include <stdint.h>
-
-// template <class T>
-// class Array
-// {
-// 	private:
-// 		Array() : T arr[];
-// 		Array(unsigned int n) : T arr[n]
-// 		{
-// 			for (unsigned int i = 0, i < n, ++i)
-// 				arr[i] = new T();
-// 		}
-// 		Array(Array const &array);
-// 		~Array();
-
-// 	public:
-// 		int size();
-
-// 		Array& operator= (Array const &array);
-// 		const Type & operator[](int Index) const
-// 		{
-// 			return Value[Index];
-// 		}
-// 		Type & operator[](int Index)
-// 		{
-// 			return Value[Index];
-// 		}
-// };
-
-
 #pragma once
 
 #include <iostream>
