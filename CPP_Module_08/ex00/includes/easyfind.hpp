eea71917bf65acfd53dbbb01af5dca1acfee7fac
HugooVer <hugo.vercelloneg@gmail.com>
2024-09-09 20:55:19 +0200

    working for list (not same for other contner) looking for

--- CPP_Module_08/ex00/includes/easyfind.hpp
+++ CPP_Module_08/ex00/includes/easyfind.hpp
@@ -2,3 +2,18 @@
 
 #include <iostream>
 #include <exception>
+#include <list>
+
+template <typename T> int easyfind(T& intCont, int look)
+{
+	int pos = 0;
+	int ii = intCont.size() - 1;
+
+	for (std::list<int>::reverse_iterator rit=intCont.rbegin(); rit!=intCont.rend(); ++rit)
+	{
+		if (*rit == look)
+			pos = ii;
+		--ii;
+	}
+	return pos;
+}
--- CPP_Module_08/ex00/srcs/main.cpp
+++ CPP_Module_08/ex00/srcs/main.cpp
@@ -1,2 +1,24 @@
 #include "easyfind.hpp"
 
+#include <iostream>
+#include <list>
+#include <vector>
+
+void printListInOrder(const std::list<int>& mylist) {
+	std::cout << "mylist in order:";
+	for (std::list<int>::const_iterator it = mylist.begin(); it != mylist.end(); ++it) {
+		std::cout << ' ' << *it;
+	}
+	std::cout << '\n';
+}
+
+int main ()
+{
+	std::list<int> mylist;
+
+	for (int i=1; i<=5; ++i) mylist.push_back(i * 11);
+	for (int i=1; i<=5; ++i) mylist.push_back(i * 11);
+	std::cout << easyfind(mylist, 22) << std::endl;
+	printListInOrder(mylist);
+	return 0;
+}
