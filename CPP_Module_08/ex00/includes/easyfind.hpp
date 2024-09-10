61be5069f9b8cb3617dcb25e2a32fd634726b7cd
HugooVer <hugo.vercelloneg@gmail.com>
2024-09-10 18:35:31 +0200

    all good

--- CPP_Module_08/ex00/includes/easyfind.hpp
+++ CPP_Module_08/ex00/includes/easyfind.hpp
@@ -4,12 +4,12 @@
 #include <exception>
 #include <list>
 
-template <typename T> int easyfind(T& intCont, int look)
+template <typename T>int easyfind(T& intCont, int look)
 {
-	int pos = 0;
+	int pos = -1;
 	int ii = intCont.size() - 1;
 
-	for (std::list<int>::reverse_iterator rit=intCont.rbegin(); rit!=intCont.rend(); ++rit)
+	for (typename T::reverse_iterator rit = intCont.rbegin(); rit != intCont.rend(); ++rit)
 	{
 		if (*rit == look)
 			pos = ii;
--- CPP_Module_08/ex00/srcs/main.cpp
+++ CPP_Module_08/ex00/srcs/main.cpp
@@ -1,24 +1,42 @@
 #include "easyfind.hpp"
 
 #include <iostream>
+#include <time.h>
+#include <cstdlib>
 #include <list>
 #include <vector>
+#include <deque>
 
-void printListInOrder(const std::list<int>& mylist) {
-	std::cout << "mylist in order:";
-	for (std::list<int>::const_iterator it = mylist.begin(); it != mylist.end(); ++it) {
+template <typename T> void printIntContInOrder(T& intCont) {
+	for (typename T::const_iterator it = intCont.begin(); it != intCont.end(); ++it) {
 		std::cout << ' ' << *it;
 	}
 	std::cout << '\n';
 }
 
+template <typename T> void feelIntCont (T& intCont) {
+	for (int i=1; i<=10; ++i)
+		intCont.push_back((rand() % 10));
+}
+
 int main ()
 {
-	std::list<int> mylist;
 
-	for (int i=1; i<=5; ++i) mylist.push_back(i * 11);
-	for (int i=1; i<=5; ++i) mylist.push_back(i * 11);
-	std::cout << easyfind(mylist, 22) << std::endl;
-	printListInOrder(mylist);
+	srand(time(NULL));
+	int i = rand() % 10;
+	std::list<int> lst;
+	std::vector<int> vec;
+	std::deque<int> deq;
+
+	feelIntCont(lst);
+	feelIntCont(vec);
+	feelIntCont(deq);
+	std::cout << i << " is in position " << easyfind(lst, i) << " in the list:";
+	printIntContInOrder(lst);
+	std::cout << i << " is in position " << easyfind(vec, i) << " in the vector:";
+	printIntContInOrder(vec);
+	std::cout << i << " is in position " << easyfind(deq, i) << " in the deque:";
+	printIntContInOrder(deq);
+
 	return 0;
 }
