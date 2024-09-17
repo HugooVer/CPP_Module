5dac45d7979617e38c0f9f4441646c97bd100439
HugooVer <hugo.vercelloneg@gmail.com>
2024-09-17 20:33:50 +0200

    creation of a new multiset as a multiset child

---
+++ CPP_Module_08/ex01/includes/LimitedMultiset.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <set>
+
+template <typename T> class LimitedMultiset : public std::multiset<T> {
+private:
+	std::size_t _max_size;
+
+public:
+	LimitedMultiset(std::size_t maxSize) : _max_size(maxSize) {}
+
+	LimitedMultiset(const LimitedMultiset& other) : std::multiset<T>(other), _max_size(other.max_size) {}
+
+	LimitedMultiset& operator=(const LimitedMultiset& other) {
+		if (this != &other) {
+			std::multiset<T>::operator=(other);
+			_max_size = other.max_size;
+		}
+		return *this;
+	}
+
+	std::pair<typename std::multiset<T>::iterator, bool> insert(const T& value) {
+		if (this->size() < _max_size) {
+			typename std::multiset<T>::iterator it = std::multiset<T>::insert(value);
+			return std::make_pair(it, true);
+		} else {
+			std::cout << "Error: Insertion failed: maximum size reached" << std::endl;
+			return std::make_pair(this->end(), false);
+		}
+	}
+
+	std::size_t max_size() const {
+		return _max_size;
+	}
+};
---
+++ CPP_Module_08/ex01/includes/Span.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <list>
+
+#include "LimitedMultiset.hpp"
+
+class Span
+{
+
+}
