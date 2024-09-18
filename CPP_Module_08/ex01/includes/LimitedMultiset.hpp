d9d8290735633a7821039566b4e9955cfca5418d
HugooVer <hugo.vercelloneg@gmail.com>
2024-09-18 16:56:56 +0200

    norm

--- CPP_Module_08/ex01/includes/LimitedMultiset.hpp
+++ CPP_Module_08/ex01/includes/LimitedMultiset.hpp
@@ -4,35 +4,35 @@
 #include <set>
 
 template <typename T> class LimitedMultiset : public std::multiset<T> {
-private:
-	std::size_t _max_size;
-
-public:
-	LimitedMultiset() : _max_size(0) {}
-
-	LimitedMultiset(std::size_t maxSize) : _max_size(maxSize) {}
-
-	LimitedMultiset(const LimitedMultiset& other) : std::multiset<T>(other), _max_size(other.max_size) {}
-
-	LimitedMultiset& operator=(const LimitedMultiset& other) {
-		if (this != &other) {
-			std::multiset<T>::operator=(other);
-			_max_size = other.max_size;
+	private:
+		std::size_t _max_size;
+	
+	public:
+		LimitedMultiset() : _max_size(0) {}
+	
+		LimitedMultiset(std::size_t maxSize) : _max_size(maxSize) {}
+	
+		LimitedMultiset(const LimitedMultiset& other) : std::multiset<T>(other), _max_size(other.max_size) {}
+	
+		LimitedMultiset& operator=(const LimitedMultiset& other) {
+			if (this != &other) {
+				std::multiset<T>::operator=(other);
+				_max_size = other.max_size;
+			}
+			return *this;
 		}
-		return *this;
-	}
-
-	std::pair<typename std::multiset<T>::iterator, bool> insert(const T& value) {
-		if (this->size() < _max_size) {
-			typename std::multiset<T>::iterator it = std::multiset<T>::insert(value);
-			return std::make_pair(it, true);
-		} else {
-			std::cout << "Error: Insertion failed: maximum size reached" << std::endl;
-			return std::make_pair(this->end(), false);
+	
+		std::pair<typename std::multiset<T>::iterator, bool> insert(const T& value) {
+			if (this->size() < _max_size) {
+				typename std::multiset<T>::iterator it = std::multiset<T>::insert(value);
+				return std::make_pair(it, true);
+			} else {
+				std::cout << "Error: Insertion failed: maximum size reached" << std::endl;
+				return std::make_pair(this->end(), false);
+			}
 		}
-	}
-
-	std::size_t max_size() const {
-		return _max_size;
-	}
+	
+		std::size_t max_size() const {
+			return _max_size;
+		}
 };
