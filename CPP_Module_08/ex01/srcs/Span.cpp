9e14a7c1815fe8105285c3e3933d2a873e3e648c
HugooVer <hugo.vercelloneg@gmail.com>
2024-10-03 21:06:15 +0200

    shortestSpan for 0

--- CPP_Module_08/ex01/srcs/Span.cpp
+++ CPP_Module_08/ex01/srcs/Span.cpp
@@ -10,7 +10,7 @@ Span::Span(int N) : mySpan(N) {
 
 Span::Span(const Span& other) : mySpan(other.mySpan) {
 	std::cout << "Span Copy Constructor Called" << std::endl;
-	}
+}
 
 Span::~Span(){}
 
@@ -18,9 +18,19 @@ void Span::addNumber(int nb){
 	mySpan.insert(nb);
 }
 
-// int Span::shortestSpan() const{
+int Span::shortestSpan() const{
+	if (mySpan.size() < 2)
+		throw std::length_error("Span have less than 2 int");
 
-// }
+	std::multiset<int>::iterator prev = mySpan.end();
+	for (std::multiset<int>::iterator it = mySpan.begin(); it != mySpan.end(); ++it) {
+		if (prev != mySpan.end() && *prev == *it)
+			return 0;
+		prev = it;
+	}
+
+	return -1;
+}
 int Span::longestSpan() const{
 	if (mySpan.size() < 2)
 		throw std::length_error("Span have less than 2 int");
--- CPP_Module_08/ex01/srcs/main.cpp
+++ CPP_Module_08/ex01/srcs/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <Span.hpp>
 int main() {
+	try
+	{
 	Span sp(5);
+	sp.addNumber(2);
 	sp.addNumber(20);
-	sp.addNumber(10);
 	sp.addNumber(50);
 	sp.addNumber(100);
 	sp.addNumber(20);
 
-	std::cout << sp.longestSpan() << std::endl;
+	std::cout << sp.shortestSpan() << std::endl;
+	// std::cout << sp.longestSpan() << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
 	return 0;
 }
