c3482340d7c0ee90d22421049f697d119265430c
HugooVer <hugo.vercelloneg@gmail.com>
2024-11-23 00:05:02 +0100

    wip

--- CPP_Module_08/ex02/includes/MutantStack.hpp
+++ CPP_Module_08/ex02/includes/MutantStack.hpp
@@ -2,9 +2,26 @@
 
 #include <iostream>
 #include <exception>
+#include <deque>
+#include <stack>
 
 template <class T>
 class MutantStack
 {
 	
 }
+
+template <typename T, class container=std::deque<T> >
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack(){}
+		~MutantStack(){}
+		MutantStack(const MutantStack &stack)
+			*this = stack;
+		typedef typename container::iterator iterator;
+		iterator begin()
+			return this->c.begin();
+		iterator end()
+			return this->c.end();
+};
